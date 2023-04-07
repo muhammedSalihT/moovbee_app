330775c5933634f94666123ab5316ab942b732bf
Salih T <mhdsalih9656@gmail.com>
2023-04-07 10:36:16 +0530

    login model added

---
+++ lib/screens/models/login_model.dart
@@ -0,0 +1,35 @@
+class LoginModel {
+  LoginModel({
+    required this.status,
+    required this.name,
+    required this.refresh,
+    required this.access,
+    required this.message,
+    required this.urlId,
+  });
+
+  bool status;
+  String name;
+  String refresh;
+  String access;
+  String message;
+  String urlId;
+
+  factory LoginModel.fromJson(Map<String, dynamic> json) => LoginModel(
+        status: json["status"],
+        name: json["name"],
+        refresh: json["refresh"],
+        access: json["access"],
+        message: json["message"],
+        urlId: json["url_id"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "status": status,
+        "name": name,
+        "refresh": refresh,
+        "access": access,
+        "message": message,
+        "url_id": urlId,
+      };
+}
