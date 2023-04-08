e61f23276f1c84f9c665d26ae8595085d03c63bd
Salih T <mhdsalih9656@gmail.com>
2023-04-08 20:15:14 +0530

    driver deleting completed

--- lib/screens/view_models/login_viewmodel.dart
+++ lib/screens/view_models/login_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moovbe_app/screens/api/login_api.dart';
 import 'package:moovbe_app/screens/views/allbuslist_screen.dart';
 import 'package:moovbe_app/utils/custom_snackbar.dart';
+import 'package:moovbe_app/utils/internet_checking.dart';
 import 'package:moovbe_app/utils/routes.dart';
 import 'package:moovbe_app/utils/secure_store.dart';
 
@@ -13,31 +14,37 @@ class LoginViewModel extends ChangeNotifier {
 
   verifyUser(context) async {
     if (formKey.currentState!.validate()) {
-      userChecking = true;
-      notifyListeners();
-      final result = await LoginApi.userLogin(
-          userName: emailController.text.trim(),
-          password: passController.text.trim());
-      if (result.message == "Check Your Internet") {
-        userChecking = false;
+      final isInternet = await InternetConnectivity.checking();
+      if (isInternet) {
+        userChecking = true;
         notifyListeners();
-        SnackBarWidget.getSnackBar(
-            context: context, showText: "Check Your Internet");
-      }
-      if (result.status == true) {
-        await SecureStore.saveToken(result.access.toString());
-        await SecureStore.saveApiKey(result.urlId.toString());
-        await SecureStore.getApiKey();
-        SnackBarWidget.getSnackBar(context: context, showText: result.message);
-        userChecking = false;
-        notifyListeners();
-        RoutesManager.removeScreen(screen: const AllBusListScreen());
-      }
-      if (result.message == "invalid credentials!") {
-        userChecking = false;
-        notifyListeners();
-        SnackBarWidget.getSnackBar(
-            context: context, showText: "invalid credentials");
+        final result = await LoginApi.userLogin(
+            userName: emailController.text.trim(),
+            password: passController.text.trim());
+        if (result.message == "Check Your Internet") {
+          userChecking = false;
+          notifyListeners();
+          SnackBarWidget.getSnackBar(
+              context: context, showText: "Check Your Internet");
+        }
+        if (result.status == true) {
+          await SecureStore.saveToken(result.access.toString());
+          await SecureStore.saveApiKey(result.urlId.toString());
+          await SecureStore.getApiKey();
+          SnackBarWidget.getSnackBar(
+              context: context, showText: result.message);
+          userChecking = false;
+          notifyListeners();
+          RoutesManager.removeScreen(screen: const AllBusListScreen());
+        }
+        if (result.message == "invalid credentials!") {
+          userChecking = false;
+          notifyListeners();
+          SnackBarWidget.getSnackBar(
+              context: context, showText: "invalid credentials");
+        }
+      } else {
+        SnackBarWidget.getSnackBar(context: context, showText: "No Internet");
       }
     } else {
       SnackBarWidget.getSnackBar(
